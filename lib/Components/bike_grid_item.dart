998ebb2a2efb56699486bbb64182c2ce21b0040a
charleston <charles1235223@gmail.com>
2021-09-05 18:21:57 -0400

    Linha secundaria

--- lib/Components/bike_grid_item.dart
+++ lib/Components/bike_grid_item.dart
@@ -3,7 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../app_routes.dart';
-
+//
 class BikeGridItem extends StatelessWidget {
 
   final Bike bike;
